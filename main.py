72e230a655c48005baef85f0edeae9eb8ee6f211
Fahad Saleh <ecsumed@yahoo.com>
2019-12-07 16:14:09 +0500

    Add words dictionary

---
+++ main.py
@@ -0,0 +1,33 @@
+from time import time
+import sys
+
+word_dictionary = []
+# load the dictionary file into the word_dictionary
+for word in open('words.txt', 'r'):
+    word_dictionary.append(word.lower().strip())
+
+
+def anagram_finder(anagram):
+    current_time = time()
+    words = []
+
+    is_anagram_found = False
+    current_time = time()
+    for word in word_dictionary:
+        if (len(word) == len(anagram)) \
+           and (sorted(anagram) == sorted(word)) \
+           and not (word == anagram):
+            is_anagram_found = True
+            words.append(word)
+
+    if not is_anagram_found:
+        words.append("No anagrams found")
+
+    return (
+        words,
+        "{}s".format(str(time() - current_time))
+    )
+
+# only run the program if started directly, rather than from a module
+if __name__ == "__main__":
+    print(anagram_finder(sys.argv[1]))
